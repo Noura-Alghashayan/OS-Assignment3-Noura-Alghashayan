a698f0d1ac84c114d4703436985df3afa44c7512
Noura-Alghashayan <445051981@std.psau.edu.sa>
2026-05-01 14:25:01 +0300

    Add ReentrantLock import

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Random;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 // ANSI Color Codes for enhanced terminal output
 class Colors {
